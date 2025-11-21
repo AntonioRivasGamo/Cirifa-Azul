f3fb9e15f00adc448e36f78e5324aab6d3d2cbce
Martin Alpuente Silva <martin.alpuentesilva@gmail.com>
2025-11-21 17:52:42 +0100

    Commit Inicial Spring

---
+++ src/test/java/com/cirifa_azul/adoption/AdoptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cirifa_azul.adoption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
