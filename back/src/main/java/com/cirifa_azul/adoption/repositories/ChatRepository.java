62c0e8c40d23640c3ad06686637b94c4d638c65f
AntonioRivasGamo <rivasgamoantonio@gmail.com>
2025-12-01 17:15:44 +0100

    Animal and Post Services added and hotfixes

--- back/src/main/java/com/cirifa_azul/adoption/repositories/ChatRepository.java
+++ back/src/main/java/com/cirifa_azul/adoption/repositories/ChatRepository.java
@@ -5,9 +5,11 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cirifa_azul.adoption.domain.entities.Chat;
 
+@Repository
 public interface ChatRepository extends JpaRepository<Chat, UUID>{
 
 }
--- back/src/main/java/com/cirifa_azul/adoption/repositories/FavouriteRepository.java
+++ back/src/main/java/com/cirifa_azul/adoption/repositories/FavouriteRepository.java
@@ -3,9 +3,11 @@ package com.cirifa_azul.adoption.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cirifa_azul.adoption.domain.entities.Favourite;
 
+@Repository
 public interface FavouriteRepository extends JpaRepository<Favourite, UUID>{
 
 }
