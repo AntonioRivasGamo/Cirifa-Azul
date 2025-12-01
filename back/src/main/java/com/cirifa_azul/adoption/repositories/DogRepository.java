414b3ce7e9e98aa779a5417a122e3eaa57f0f788
AntonioRivasGamo <rivasgamoantonio@gmail.com>
2025-12-01 22:11:11 +0100

    Dog repository and service added

--- back/src/main/java/com/cirifa_azul/adoption/repositories/DogRepository.java
+++ back/src/main/java/com/cirifa_azul/adoption/repositories/DogRepository.java
@@ -3,9 +3,10 @@ package com.cirifa_azul.adoption.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.cirifa_azul.adoption.domain.entities.Dog;
 
-public interface DogRepository extends JpaRepository<Dog, UUID>{
+public interface DogRepository extends JpaRepository<Dog, UUID>, JpaSpecificationExecutor<Dog>{
 
 }
---
+++ back/src/main/java/com/cirifa_azul/adoption/services/impl/DogServiceImpl.java
@@ -0,0 +1,60 @@
+package com.cirifa_azul.adoption.services.impl;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.cirifa_azul.adoption.domain.entities.Dog;
+import com.cirifa_azul.adoption.domain.entities.enums.Gender;
+import com.cirifa_azul.adoption.domain.entities.enums.HairLength;
+import com.cirifa_azul.adoption.domain.entities.enums.Size;
+import com.cirifa_azul.adoption.repositories.DogRepository;
+import com.cirifa_azul.adoption.repositories.specifications.DogSpecification;
+import com.cirifa_azul.adoption.services.DogService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class DogServiceImpl implements DogService{
+
+	private final DogRepository dogRepository;
+	
+	@Override
+	public List<Dog> findAll() {
+		return dogRepository.findAll();
+	}
+
+	@Override
+	public Optional<Dog> findById(UUID id) {
+		return dogRepository.findById(id);
+	}
+
+	@Override
+	public Dog create(Dog dog) {
+		return dogRepository.save(dog);
+	}
+
+	@Override
+	public Optional<Dog> update(Dog dog) {
+		return dogRepository.findById(dog.getId()).map(d -> dogRepository.save(dog));
+	}
+
+	@Override
+	public Boolean delete(UUID id) {
+		if(dogRepository.existsById(id)) {
+			dogRepository.deleteById(id);
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public List<Dog> filterDogList(Integer age, String breed, Gender gender, HairLength hairLength, 
+			Boolean isCastrated, Boolean isVaccinated, String name, Size size, Double weight) {
+		return dogRepository.findAll(DogSpecification.filterDogs(age, breed, gender, hairLength, isCastrated, 
+				isVaccinated, name, size, weight));
+	}
+}
